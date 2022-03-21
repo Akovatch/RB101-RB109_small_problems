d40e4aff2e0803622d902479a45a00c3a243f2ad
Tony Kovatch <Tonyaccordionist@gmail.com>
2022-03-21 19:53:08 -0400

    easy_9

--- easy_9/3_always_return_negative.rb
+++ easy_9/3_always_return_negative.rb
@@ -1,6 +1,6 @@
 # Write a method that takes a number as an argument. If the
 # argument is a positive number, return the negative of that
-# number. If the number is 0 or negative, return the original number.
+# number. If the number is zero or negative, return the original number.
 
 def negative(num)
   num > 0 ? -num : num
