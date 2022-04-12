e3a4dfe60fd929fa47799ea2403130c7b8cc6c4c
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-11 20:08:59 -0400

    1_10

---
+++ medium_1/medium_1_10.rb
@@ -0,0 +1,45 @@
+# Fibonacci Numbers (Last Digit)
+# In the previous exercise, we developed a procedural method for computing the
+# value of the nth Fibonacci numbers. This method was really fast, computing
+# the 20899 digit 100,001st Fibonacci sequence almost instantly.
+
+# In this exercise, you are going to compute a method that returns the last
+# digit of the nth Fibonacci number.
+
+# Examples:
+
+# fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+# fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+# fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+# fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+# fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+# fibonacci_last(123456789) # -> 4
+
+# this is the solution LS expected you to come up with...however, it is undable to compute the last digit past
+# the 1,000,000th number.
+
+def fibonacci_last(nth)
+  fibonacci(nth).to_s.chars.pop.to_i
+end
+
+def fibonacci(nth)
+  first, last = [1, 1]
+  3.upto(nth) do
+    first, last = [last, first + last]
+  end
+
+  last
+end
+
+
+# LS solution: This is able to computer very far down the sequence, above the 1,000,000th number. It reduces numbers
+# down to their last digit only during each calculation, which saves memory.
+
+def fibonacci_last(nth)
+  last_2 = [1, 1]
+  3.upto(nth) do
+    last_2 = [last_2.last, (last_2.first + last_2.last) % 10]
+  end
+
+  last_2.last
+end
