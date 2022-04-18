f3c0bf5a3bad1cf791a921710245e91f1c08e0ce
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-18 09:35:38 -0400

    adding medium_1_3

--- medium_1/medium_1_3.rb
+++ medium_1/medium_1_3.rb
@@ -19,70 +19,24 @@
 # max_rotation(105) == 15 # the leading zero gets dropped
 # max_rotation(8_703_529_146) == 7_321_609_845
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def max_rotation(num)
   counter = num.to_s.length
-  while counter >= 2
+  loop do
+    break if counter == 1
     num = rotate_rightmost_digits(num, counter)
     counter -= 1
   end
   num
 end
 
-def rotate_rightmost_digits(number, n)
-  all_digits = number.to_s.chars
-  all_digits[-n..-1] = rotate_array(all_digits[-n..-1])
-  all_digits.join.to_i
+def rotate_rightmost_digits(num, n)
+  num = num.to_s.chars
+  num[-n..-1] = rotate_array(num[-n..-1])
+  num.join.to_i
 end
 
 def rotate_array(array)
-  array[1..-1] + [array[0]]
+  array[1..-1] << array[0]
 end
 
 p max_rotation(735291) == 321579
@@ -90,6 +44,7 @@ p max_rotation(3) == 3
 p max_rotation(35) == 53
 p max_rotation(105) == 15 # the leading zero gets dropped
 p max_rotation(8_703_529_146) == 7_321_609_845
+
 
 # LS solution: using downto looping mechanism
 
