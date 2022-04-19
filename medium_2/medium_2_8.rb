811c2c4404459206ba353081afc9302e1bbde812
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-19 08:36:34 -0400

    Update medium_2_8.rb

--- medium_2/medium_2_8.rb
+++ medium_2/medium_2_8.rb
@@ -17,66 +17,29 @@
 
 # featured(9_999_999_999) # -> There is no possible number that fulfills those requirements
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# input: integer
+# output: integer (next greatest featured number)
+# rules:
+  # odd, mult of 7, and unique digits
+  # return next featured number
+  # return error message if there is no featured number !!!
+# alg:
+  # 1.upto(7) get next odd multiple of 7
+  # count up from input (1.upto(1_023_456_987)
+  #  unique digits (digits == digits.uniq)
+    # increment number up by 14
 
 def featured(num)
-  num += 1
-  num += 1 until num.odd? && num % 7 == 0
-  loop do
-    return num if num.to_s.chars == num.to_s.chars.uniq
-    num += 14
-    break if num >= 9_876_543_210
+  starting_num = 0
+  1.upto(14) do |n|
+    starting_num = (num + n) if (num + n) % 7 == 0 && (num + n).odd?
   end
-  'There is no possible number that fulfills those requirements.'
+  while starting_num <= 1_023_456_987
+    return starting_num if starting_num.digits == starting_num.digits.uniq
+    starting_num += 14
+  end
+  'There is no possible number that fulfills those requirements'
 end
-
-p featured(870)
 
 p featured(12) == 21
 p featured(20) == 21
@@ -85,20 +48,20 @@ p featured(1029) == 1043
 p featured(999_999) == 1_023_547
 p featured(999_999_987) == 1_023_456_987
 
-# My original solution broke the terminal after 900 :(
+# # My original solution broke the terminal after 900 :(
 
-# #LS solution:
+# # #LS solution:
 
-  def featured(number)
-    number += 1
-    number += 1 until number.odd? && number % 7 == 0
+#   def featured(number)
+#     number += 1
+#     number += 1 until number.odd? && number % 7 == 0
 
-    loop do
-      number_chars = number.to_s.split('')
-      return number if number_chars.uniq == number_chars
-      number += 14
-      break if number >= 9_876_543_210
-    end
+#     loop do
+#       number_chars = number.to_s.split('')
+#       return number if number_chars.uniq == number_chars
+#       number += 14
+#       break if number >= 9_876_543_210
+#     end
 
-    'There is no possible number that fulfills those requirements.'
-  end
+#     'There is no possible number that fulfills those requirements.'
+#   end
